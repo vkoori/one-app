2b65c074c230e0c9a7c3990edcbb2fa4d613166b
koorosh safeashrafi <k.seif@kargosha.com>
2022-10-14 02:29:38 +0330

    modify shell request

--- App/swoole.php
+++ App/swoole.php
@@ -10,7 +10,7 @@ define('_APP_PATH_VIEW_', __DIR__ . '/View');
 //define('_DEBUG_',true);
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/lizhichao/one/src/run.php';
+require __DIR__ . '/../vendor/vkoori/one/src/run.php';
 require __DIR__ . '/config.php';
 
 
